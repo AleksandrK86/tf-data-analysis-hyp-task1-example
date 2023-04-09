93a0745c69a4e4b40c132979fb9dbd69baef331f
Aleksandr Karpukhin <116599912+AleksandrK86@users.noreply.github.com>
2023-04-09 22:58:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,17 +12,9 @@ def solution(x_success: int,
   
     alpha=0.02
     
-    p1 = x_success / x_cnt
-    p2 = y_success / y_cnt
-    p_combined = (x_success + y_success) / (x_cnt + y_cnt)
-    difference = p1 - p2
- 
-    z_value = difference / math.sqrt(
-        p_combined * (1 - p_combined) * (1 / x_success + 1 / y_success)
-    ) 
-    distr = stats.norm(0, 1)  
+    p_value=proportions_ztest([x_success, y_success], [x_cnt, y_cnt])[1]
     
-    p_value = (1 - distr.cdf(abs(z_value))) * 2 
+    print(p_value)
     
     if (p_value < alpha):
       return True
